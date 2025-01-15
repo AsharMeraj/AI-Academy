53fd0c901f3d6e5e2fc06885f5306ce5f48b6114
Ashar <asharmeraj55@gmail.com>
2025-01-15 19:22:15 +0500

    Fixing Error

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -5,7 +5,6 @@ export const runtime = 'edge'
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  streaming: 'allow',
   functions: [
     helloWorld,
     CreateNewUser,
