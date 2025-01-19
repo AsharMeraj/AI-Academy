eb7fe22f848167e027c687abc5b22e633992d850
Ashar <asharmeraj55@gmail.com>
2025-01-19 17:43:12 +0500

    Fixing bug

--- src/app/course/[courseId]/notes/_components/MaterialCardItem.tsx
+++ src/app/course/[courseId]/notes/_components/MaterialCardItem.tsx
@@ -175,11 +175,11 @@ const MaterialCardItem = (props: PropType) => {
         );
   
       console.log("Chapters Ready: " + result.length);
+      console.log("Chapter length: " + props.course.courseLayout.chapters.length)
   
       if (result.length < props.course.courseLayout.chapters.length) {
         // Continue polling after 2 seconds
         setTimeout(async () => {
-          await props.refreshData();
           checkNotes();
         }, 2000);
       } else {
