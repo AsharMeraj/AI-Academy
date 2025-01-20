41018b4c8f5874693a56df524a9dd963bed1b54b
Ashar <asharmeraj55@gmail.com>
2025-01-20 10:32:33 +0500

    Fixing bug

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,7 +2,6 @@ import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { CreateNewUser, GenerateNotes, GenerateStudyTypeContent, helloWorld } from "@/inngest/functions";
 // Create an API that serves zero functions
-export const runtime = 'edge'
 export const { GET, POST, PUT } = serve({
   client: inngest,
   streaming: 'allow',
